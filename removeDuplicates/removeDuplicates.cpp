84dde5e8f9a3b4885b3f08804a6e56dade90f09a
hanhan0-0 <92200209+hanhan0-0@users.noreply.github.com>
2022-01-16 18:09:48 +0800

    Create removeDuplicates.cpp

---
+++ removeDuplicates/removeDuplicates.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size())
+        {
+            if((i+1)<nums.size() && nums[i]==nums[i+1])
+                {
+                    nums.erase(nums.begin()+i);
+                    continue;
+                }
+            i++;
+        }
+        return nums.size();
+    }
+};
