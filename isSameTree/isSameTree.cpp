1b5145f051d9d95da0a2494f6d8b80fa0a1e2f62
hanhan0-0 <92200209+hanhan0-0@users.noreply.github.com>
2022-01-23 23:39:13 +0800

    Create isSameTree.cpp

---
+++ isSameTree/isSameTree.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(inorderTraversal(p)==inorderTraversal(q))
+            return true;
+        return false;
+    }
+    vector<int> inorderTraversal(TreeNode* root) {
+       
+        vector<int>a;
+        zhong(root,a);
+        return a;
+    }
+    void zhong(TreeNode* root,vector<int>&a) 
+    {
+         if(root==nullptr)
+            return ;
+         zhong(root->left,a);
+         a.push_back(root->val);
+         zhong(root->right,a);
+    }
+};
