3fe02beb1f3de912fb8a333dd4bed0f5f91d7802
hanhan0-0 <92200209+hanhan0-0@users.noreply.github.com>
2022-01-27 23:30:34 +0800

    Create singleNumber.cpp

---
+++ singleNumber/singleNumber.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        int i=0;
+        while(i<nums.size())
+        {
+            if(i+1<nums.size() && nums[i]==nums[i+1])
+            {
+                i+=2;
+            }
+            else
+                return nums[i];
+        }
+        return 0;
+
+    }
+};
