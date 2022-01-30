ad859922226b176e327b00bcfff6b0fc3cd4a690
hanhan0-0 <92200209+hanhan0-0@users.noreply.github.com>
2022-01-30 17:22:02 +0800

    Create findMaxConsecutiveOnes.cpp

---
+++ findMaxConsecutiveOnes/findMaxConsecutiveOnes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<nums.size();i++)
+        {
+           if(nums[i]==1)
+           {
+               sum++;
+               ans=ans>sum?ans:sum;
+           }
+           else if(nums[i]==0)
+           {
+               sum=0;
+           }
+        }
+        return ans;
+
+    }
+};
